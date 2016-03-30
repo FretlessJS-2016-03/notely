0033a3a096d9cd4fce186678ea9b309b0f8f7e77
Dave Strus <dave@getfretless.com>
2016-03-30 13:38:25 -0400

    Retrieve notes from MongoDB.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var notelyServerApp = express();
 
+var db = require('mongoose');
+db.connect('mongodb://localhost:27017/notely');
+
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String
+});
+
+var Note = db.model('Note', NoteSchema);
+
 // Cross-Origin Resource Sharing (CORS) middleware
 notelyServerApp.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -8,16 +18,9 @@ notelyServerApp.use(function(req, res, next) {
 });
 
 notelyServerApp.get('/', function(req, res) {
-  res.json([
-    {
-      title: 'Edited hardcoded note!',
-      body_html: 'Cool note. Aww, shucks.'
-    },
-    {
-      title: 'Another edited hardcoded note',
-      body_html: "Ain't life grand?"
-    }
-  ]);
+  Note.find().then(function(notes) {
+    res.json(notes);
+  });
 });
 
 notelyServerApp.listen(3030, function() {
