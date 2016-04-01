f23e43d9196b9f0977bbf547d2cef783306da368
Dave Strus <dave@getfretless.com>
2016-04-01 11:22:05 -0400

    Add signUp directive and template.

--- client/app/content/bundle.js
+++ client/app/content/bundle.js
@@ -14,52 +14,11 @@
 })();
 'use strict';
 
-(function () {
-  angular.module('notely.notes', ['ui.router', 'textAngular']).config(notesConfig);
-
-  notesConfig.$inject = ['$stateProvider'];
-  function notesConfig($stateProvider) {
-    $stateProvider.state('notes', {
-      url: '/notes',
-      templateUrl: '/notes/notes.html',
-      controller: NotesController,
-      resolve: {
-        notesLoaded: function notesLoaded(NotesService) {
-          return NotesService.fetch();
-        }
-      }
-    }).state('notes.form', {
-      url: '/:noteId',
-      templateUrl: '/notes/notes-form.html',
-      controller: NotesFormController
-    });
-  }
-
-  NotesController.$inject = ['$scope', '$state', 'NotesService'];
-  function NotesController($scope, $state, NotesService) {
-    $scope.notes = NotesService.getNotes();
-    $state.go('notes.form');
-  }
-
-  NotesFormController.$inject = ['$scope', '$state', 'NotesService'];
-  function NotesFormController($scope, $state, NotesService) {
-    $scope.note = NotesService.findById($state.params.noteId);
-    $scope.save = function () {
-      if ($scope.note._id) {
-        NotesService.update($scope.note);
-      } else {
-        NotesService.create($scope.note).then(function (response) {
-          $state.go('notes.form', { noteId: response.data.note._id });
-        });
-      }
-    };
-    $scope['delete'] = function () {
-      NotesService['delete']($scope.note).then(function () {
-        $state.go('notes.form', { noteId: undefined });
-      });
-    };
-  }
-})();
+angular.module('notely').directive('signUp', function () {
+  return {
+    templateUrl: '/components/sign-up.html'
+  };
+});
 'use strict';
 
 (function () {
@@ -142,6 +101,67 @@
         }
       }
       return {};
+    };
+  }
+})();
+'use strict';
+
+{
+  var usersConfig = function usersConfig($stateProvider) {
+    $stateProvider.state('sign-up', {
+      url: '/sign-up',
+      template: '<sign-up></sign-up>'
+    });
+  };
+  usersConfig.$inject = ['$stateProvider'];
+
+  angular.module('notely').config(usersConfig);
+}
+'use strict';
+
+(function () {
+  angular.module('notely.notes', ['ui.router', 'textAngular']).config(notesConfig);
+
+  notesConfig.$inject = ['$stateProvider'];
+  function notesConfig($stateProvider) {
+    $stateProvider.state('notes', {
+      url: '/notes',
+      templateUrl: '/notes/notes.html',
+      controller: NotesController,
+      resolve: {
+        notesLoaded: function notesLoaded(NotesService) {
+          return NotesService.fetch();
+        }
+      }
+    }).state('notes.form', {
+      url: '/:noteId',
+      templateUrl: '/notes/notes-form.html',
+      controller: NotesFormController
+    });
+  }
+
+  NotesController.$inject = ['$scope', '$state', 'NotesService'];
+  function NotesController($scope, $state, NotesService) {
+    $scope.notes = NotesService.getNotes();
+    $state.go('notes.form');
+  }
+
+  NotesFormController.$inject = ['$scope', '$state', 'NotesService'];
+  function NotesFormController($scope, $state, NotesService) {
+    $scope.note = NotesService.findById($state.params.noteId);
+    $scope.save = function () {
+      if ($scope.note._id) {
+        NotesService.update($scope.note);
+      } else {
+        NotesService.create($scope.note).then(function (response) {
+          $state.go('notes.form', { noteId: response.data.note._id });
+        });
+      }
+    };
+    $scope['delete'] = function () {
+      NotesService['delete']($scope.note).then(function () {
+        $state.go('notes.form', { noteId: undefined });
+      });
     };
   }
 })();
