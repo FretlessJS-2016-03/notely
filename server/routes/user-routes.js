35e03bb87f0f13e0c3500f08121cd0d2dafd2263
Dave Strus <dave@getfretless.com>
2016-04-02 16:54:15 -0400

    Store encrypted passwords.

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,10 +1,14 @@
+var bcrypt = require('bcryptjs');
 var router = require('express').Router();
 var User = require('../models/user');
 
 router.post('/', function(req, res) {
+  var salt = bcrypt.genSaltSync(10);
+
   var user = new User({
     username: req.body.user.username,
-    name: req.body.user.name
+    name: req.body.user.name,
+    password_digest: bcrypt.hashSync(req.body.user.password, salt)
   });
 
   user.save().then(function(userData) {
