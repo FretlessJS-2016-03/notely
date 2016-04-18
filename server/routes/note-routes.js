97e0a547b8c0c99ac221f71d9e79da340dc5ccb2
Dave Strus <dave@getfretless.com>
2016-04-18 19:44:41 -0400

    Send appropriate messages from server when saving notes.

--- server/routes/note-routes.js
+++ server/routes/note-routes.js
@@ -16,24 +16,38 @@ router.post('/', function(req, res) {
     user: req.user
   });
 
-  note.save().then(function(noteData) {
-    res.json({
-      message: 'Saved!',
-      note: noteData
-    });
-  });
+  note.save().then(
+    function(noteData) {
+      res.json({
+        message: 'Saved!',
+        note: noteData
+      });
+    },
+    function() {
+      res.status(500).json({
+        message: 'Oops! There was problem creating that note.'
+      });
+    }
+  );
 });
 
 router.put('/:noteId', function(req, res) {
   Note.findOne({ _id: req.params.noteId, user: req.user }).then(function(note) {
     note.title = req.body.note.title;
     note.body_html = req.body.note.body_html;
-    note.save().then(function() {
-      res.json({
-        message: 'Saved!',
-        note: note
-      });
-    });
+    note.save().then(
+      function() {
+        res.json({
+          message: 'Saved!',
+          note: note
+        });
+      },
+      function() {
+        res.status(500).json({
+          message: 'Oops! There was a problem updating that note.'
+        });
+      }
+    );
   });
 });
 
